57febb66285fe6f685e018a93aae051a632f8e2b
yerboygiles <43770337+yerboygiles@users.noreply.github.com>
2020-07-17 13:15:04 -0700

    Create methods.py

---
+++ methods.py
@@ -0,0 +1,42 @@
+import math
+# distance function ergh, doesn't quite work. so close
+def distance1(maxx, minx, maxy, miny):
+    # my math is horrid and it doesn't work
+    # To find the CONSTANT_distancetocm variable, I basically had to manually measure
+    # distance of objects from the camera, then divided the distance from the camera with a number gained
+    # from the equation (m/(1-(a/b))) m - distance between when a and b were measured, a -
+    SizeX = (maxx - minx)
+    SizeY = (maxy - miny)
+    pixeltomm = 145.6 / SizeY
+    ObjectCenterX = (minx - maxx) / 2
+    CONSTANT_distancetocm = 698
+    OffCenterX = int(maxx - (SizeX / 2)) - int(imageWidth / 2)
+    OffCenterY = int(maxy - (SizeY / 2)) - int(imageHeight / 2)
+    DistanceC = math.sqrt(pow(OffCenterX, 2) + pow(OffCenterY, 2))
+    DistanceA = ((TARGETYSIZEMM * FOCALLENGTH) / SizeY)
+    DistanceB = math.sqrt(pow(DistanceA, 2) + pow(DistanceC, 2))
+    Distance = ((TARGETYSIZEMM * FOCALLENGTH) / SizeY)
+    return LateralDistance, Distance, OffCenterX, OffCenterY
+
+# not being used, maybe make another equation later on
+def distance2(maxx, minx, maxy, miny):
+    SizeX = (maxx - minx)
+    SizeY = (maxy - miny)
+    OffCenterX = int(maxx - (SizeX / 2)) - int(imageWidth / 2)
+    OffCenterY = int(maxy - (SizeY / 2)) - int(imageHeight / 2)
+    magnification = TARGETYSIZEMM / (maxy - miny)
+    Distance = FOCALLENGTH / magnification
+    DistanceMM = Distance / 25.4
+    return LateralDistance, Distance, OffCenterX, OffCenterY
+
+# this is the only one that works, has golden number(my number)
+def distance3(maxx, minx, maxy, miny):
+    THEODORS_NUMBER = 0.0516657316
+    SizeX = (maxx - minx)
+    SizeY = (maxy - miny)
+    OffCenterX = int(maxx - (SizeX / 2)) - int(imageWidth / 2)
+    OffCenterY = int(maxy - (SizeY / 2)) - int(imageHeight / 2)
+    Distance = (TARGETYSIZEMM * FOCALLENGTH) / SizeY
+    Distance = Distance/THEODORS_NUMBER
+    return LateralDistance, Distance, OffCenterX, OffCenterY
+
